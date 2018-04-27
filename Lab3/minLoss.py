39ad29d8df5bb3fc91271be66f34318734e68647
Your Name <you@example.com>
2018-04-27 11:02:28 +0800

    count loss.

---
+++ Lab3/minLoss.py
@@ -0,0 +1,9 @@
+import pickle
+
+with open('./log_st/histories_st.pkl', 'rb') as f:
+	data = pickle.load(f)
+
+lt = data['loss_history']
+min_loss = min(lt.items(), key=lambda lt: lt[1])
+print(min_loss[1])
+
