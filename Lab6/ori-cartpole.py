e6728db3f940ec140c5ffdb62fae2d8fe4d11fd2
Your Name <you@example.com>
2018-06-13 10:20:41 +0800

    Lab6 temp ok

--- Lab6/ori-cartpole.py
+++ Lab6/ori-cartpole.py
@@ -15,18 +15,15 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-DEBUG = False
-
 # hyper parameters
-EPISODES = 1000  # number of episodes
-EPS_START = 1  # e-greedy threshold start value
-EPS_END = 0.1  # e-greedy threshold end value
-EPS_DECAY = 0.995  # e-greedy threshold decay
+EPISODES = 200  # number of episodes
+EPS_START = 0.9  # e-greedy threshold start value
+EPS_END = 0.05  # e-greedy threshold end value
+EPS_DECAY = 200  # e-greedy threshold decay
 GAMMA = 0.8  # Q-learning discount factor
 LR = 0.001  # NN optimizer learning rate
-HIDDEN_LAYER = 32  # NN hidden layer size
-BATCH_SIZE = 128  # Q-learning batch size
-Memory_Capacity = 5000
+HIDDEN_LAYER = 256  # NN hidden layer size
+BATCH_SIZE = 64  # Q-learning batch size
 
 # if gpu is to be used
 use_cuda = torch.cuda.is_available()
@@ -58,30 +55,20 @@ class Network(nn.Module):
         nn.Module.__init__(self)
         self.l1 = nn.Linear(4, HIDDEN_LAYER)
         self.l2 = nn.Linear(HIDDEN_LAYER, 2)
-        #self.l2 = nn.Linear(HIDDEN_LAYER, 16)
-        self.l3 = nn.Linear(16, 2)
 
     def forward(self, x):
         x = F.relu(self.l1(x))
-        #x = F.relu(self.l2(x))
         x = self.l2(x)
-        #x = self.l3(x)
         return x
 
 
 env = gym.make('CartPole-v0')
-env._max_episode_steps = 1000
 env = wrappers.Monitor(env, './tmp/cartpole-v0-1', force=True)
 
-
 model = Network()
-targetModel = Network()
 if use_cuda:
     model.cuda()
-    targetModel.cuda()
-
-targetModel.eval()
-memory = ReplayMemory(Memory_Capacity)
+memory = ReplayMemory(10000)
 optimizer = optim.Adam(model.parameters(), LR)
 steps_done = 0
 episode_durations = []
@@ -92,9 +79,6 @@ def select_action(state):
     sample = random.random()
     eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
     steps_done += 1
-    if(DEBUG):
-        print('state:')
-        print state
     if sample > eps_threshold:
         return model(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
     else:
@@ -107,12 +91,7 @@ def run_episode(e, environment):
     while True:
         environment.render()
         action = select_action(FloatTensor([state]))
-        if(DEBUG):
-            print('action:')
-            print(action)
-
         next_state, reward, done, _ = environment.step(action[0, 0])
-        # next_state = [Cart Position, Cart Velocity, Pole Angle, Pole Velocity At Tip]
 
         # negative reward when attempt ends
         if done:
@@ -123,33 +102,15 @@ def run_episode(e, environment):
                      FloatTensor([next_state]),
                      FloatTensor([reward])))
 
-        # Update target network every 50 iterations
         learn()
-
-        if(e % 50 == 0):
-
-            #torch.save(model, 'originalNetwork.pt')
-            #targetModel = torch.load('originalNetwork.pt')
-            targetModel.load_state_dict(model.state_dict())
-
-            '''
-            print('after update, Target Network:')
-            for name, param in targetModel.named_parameters():
-                if param.requires_grad:
-                    if(name == 'l2.weight'):
-                        print name, param.data[0][0:10]
-            '''
 
         state = next_state
         steps += 1
 
-        if(e > 40 and DEBUG):
-            raw_input()
-
         if done:
             print("{2} Episode {0} finished after {1} steps"
                   .format(e, steps, '\033[92m' if steps >= 195 else '\033[99m'))
-            episode_durations.append(steps - 2) # cause the last step's reward is -1
+            episode_durations.append(steps)
             plot_durations()
             break
 
@@ -170,17 +131,8 @@ def learn():
     # current Q values are estimated by NN for all actions
     current_q_values = model(batch_state).gather(1, batch_action)
     # expected Q values are estimated from actions which gives maximum Q value
-    #max_next_q_values = model(batch_next_state).detach().max(1)[0]
-    max_next_q_values = targetModel(batch_next_state).detach().max(1)[0]
+    max_next_q_values = model(batch_next_state).detach().max(1)[0]
     expected_q_values = batch_reward + (GAMMA * max_next_q_values)
-
-    '''
-    print('after update, Target Network:')
-    for name, param in targetModel.named_parameters():
-        if param.requires_grad:
-            if(name == 'l2.weight'):
-                print name, param.data[0][0:10]
-    '''
 
     # loss is measured from error between current and newly expected Q values
     loss = F.smooth_l1_loss(current_q_values, expected_q_values)
@@ -197,17 +149,13 @@ def plot_durations():
     durations_t = torch.FloatTensor(episode_durations)
     plt.title('Training...')
     plt.xlabel('Episode')
-    plt.ylabel('Reward')
+    plt.ylabel('Duration')
     plt.plot(durations_t.numpy())
-    plt.savefig('result.png')
-
-    '''
     # take 100 episode averages and plot them too
     if len(durations_t) >= 100:
         means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
         means = torch.cat((torch.zeros(99), means))
         plt.plot(means.numpy())
-    '''
 
     plt.pause(0.001)  # pause a bit so that plots are updated
 
@@ -218,6 +166,5 @@ for e in range(EPISODES):
 print('Complete')
 env.render(close=True)
 env.close()
-#plt.ioff()
-#plt.show()
-plt.savefig('result.png')
+plt.ioff()
+plt.show()
